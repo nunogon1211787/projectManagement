c99403a340ab69b2397d8b46b7e68b09cf590c24
Ivan Araujo de Aguiar <1211768@isep.ipp.pt>
2021-12-30 15:54:00 +0000

    criação das classes Profile, SystemUser com contrutores e SystemUser com testes do construtor e a classe User Profile

---
+++ src/main/java/switch2021/project/model/SystemUser.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+
+public class SystemUser {
+
+    //atributes
+    private String userName;
+    private String email;
+    private String photo;
+    private String password;
+    private boolean activateUser;
+
+    //contructor without photo
+    public SystemUser(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.photo = "";
+        this.password = password;
+        this.activateUser = false;
+    }
+
+
+    public SystemUser(String userName, String email, String photo, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.photo = photo;
+        this.password = password;
+        this.activateUser = false;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getPhoto() {
+        return this.photo;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean activateUser() {
+        return true;
+    }
+}
