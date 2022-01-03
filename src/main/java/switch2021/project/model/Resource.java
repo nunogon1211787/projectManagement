17858dd89c67fbdb0aca9379b88329f3c840b63d
Caroli1211753 <1211753@isep.ipp.pt>
2022-01-03 11:26:45 +0100

    actualização de resource e resourcetest ainda sem testes

--- src/main/java/switch2021/project/model/Resource.java
+++ src/main/java/switch2021/project/model/Resource.java
@@ -1,2 +1,34 @@
-package switch2021.project.model;public class Resource {
+package switch2021.project.model;
+
+public class Resource {
+    /**
+     * Atributos da classe Resource
+     **/
+    private String userEmail;
+    private String projectCode;
+
+    private CharSequence startDate; /** è necessario mudar as datas o mesmo formato(ver qual o melhor)  **/
+    private CharSequence endDate; /** è necessario mudar as datas o mesmo formato(ver qual o melhor)  **/
+
+    private double costPerHour;
+    private double percentageOfAllocation;
+
+    /**
+     * Construtor de Resource
+     **/
+
+    public Resource(String userEmail, String projectCode, CharSequence startDate, CharSequence endDate, double costPerHour, double percentageOfAllocation){
+
+        this.userEmail = userEmail;
+        this.projectCode = projectCode;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.costPerHour = costPerHour;
+        this.percentageOfAllocation = percentageOfAllocation;
+    }
+
+    /** Métodos "Getter" dos atributos **/
+
+    //public ArrayList<Double> getAllocationList(){ return percentageOfAllocation};
+
 }
--- src/test/java/switch2021/project/model/ResourceTest.java
+++ src/test/java/switch2021/project/model/ResourceTest.java
@@ -1,4 +1,8 @@
+package switch2021.project.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ResourceTest {
-  
+
+
 }
