5b562395595ff4595654235e77ec7780b77731c1
Paulo Cunha <1211789@isep.ipp.pt>
2022-01-04 17:07:25 +0000

    Adicionado Testes

--- src/test/java/switch2021/project/model/ProjectTest.java
+++ src/test/java/switch2021/project/model/ProjectTest.java
@@ -5,13 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProjectTest {
 
 
-    /** O teste Está imcompleto ainda **/
+    /**
+     * O teste Está imcompleto ainda
+     **/
 
     @Test
     @DisplayName("Teste de criação de projecto")
@@ -20,11 +23,12 @@ class ProjectTest {
         ArrayList<String> businessSector = new ArrayList<String>();
         businessSector.add("business_1");
         businessSector.add("business_2");
-        LocalDate date = LocalDate.of(2021,12,12);
+        LocalDate date = LocalDate.of(2021, 12, 12);
 
         //
         Project newProject = new Project("123testcode", "prototype", "test", "customer_0",
-                "typology_0", businessSector, date, 7, 5000 );
+                "typology_0", businessSector, date, 7, 5000);
+        newProject.setEndDate();
 
         // Expected
         String code = newProject.getCode();
@@ -42,10 +46,13 @@ class ProjectTest {
         String typology = newProject.getTypology();
         String valueTypology = "typology_0";
 
-        ArrayList<String> businessSectorCheck = newProject.getBusinessSector();
+        List<String> businessSectorCheck = newProject.getBusinessSector();
 
         LocalDate startDate = newProject.getStartDate();
-        LocalDate valueStarDate = LocalDate.of(2021,12,12);
+        LocalDate valueStarDate = LocalDate.of(2021, 12, 12);
+
+        LocalDate endDate = newProject.getEndDate();
+        LocalDate valueEndDate = LocalDate.now();
 
         int numberOfSprints = newProject.getNumberOfSprints();
         int valueNumberOfSprints = 7;
@@ -57,17 +64,18 @@ class ProjectTest {
         String valueStatus = "Status_0";
 
         // Result
-        assertEquals(valueCode,code);
-        assertEquals(valueName,name);
-        assertEquals(valueDescription,description);
-        assertEquals(valueCustomer,customer);
-        assertEquals(valueTypology,typology);
-        assertEquals(valueStarDate,startDate);
-        assertEquals(valueNumberOfSprints,numberOfSprints);
-        assertEquals(valueBudget,budget);
-        assertEquals(valueStatus,status);
+        assertEquals(valueCode, code);
+        assertEquals(valueName, name);
+        assertEquals(valueDescription, description);
+        assertEquals(valueCustomer, customer);
+        assertEquals(valueTypology, typology);
+        assertEquals(valueStarDate, startDate);
+        assertEquals(valueEndDate, endDate);
+        assertEquals(valueNumberOfSprints, numberOfSprints);
+        assertEquals(valueBudget, budget);
+        assertEquals(valueStatus, status);
+        assertTrue(businessSector.equals(businessSectorCheck));
 
-        /*Assert.(businessSectorCheck,businessSector);*/ /** Colocar Método de comparar Arrays **/
     }
 
 }
