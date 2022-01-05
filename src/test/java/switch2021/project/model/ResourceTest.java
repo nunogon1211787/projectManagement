f166a54647c5d1ef93920460b15e2f200c15fa26
Caroli1211753 <1211753@isep.ipp.pt>
2022-01-05 16:31:25 +0000

    actualização de ResourceTest mas está incompleto

--- src/test/java/switch2021/project/model/ResourceTest.java
+++ src/test/java/switch2021/project/model/ResourceTest.java
@@ -1,8 +1,32 @@
 package switch2021.project.model;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResourceTest {
+
+    @Test
+    @DisplayName("Teste de criação de Resource")
+    public void associateResource() {
+        //Arrange
+
+
+        LocalDate startDate = LocalDate.of(2021,12,12);
+        LocalDate endDate = LocalDate.of(2022,01,05);
+
+
+        //
+        Resource assResource = new Resource(2, "123testcode", startDate, endDate, 100, 50 );
+
+        //Act
+
+        //Assert
+
+    }
 
 
 }
