bd932e57b68048760834b66441e7c2f6d6dbc525
joanafangueiro <79871847+joanafangueiro@users.noreply.github.com>
2022-01-06 22:10:09 +0000

    Controllers (US010 e US011 - Este ainda está imcompleto).

--- src/main/java/switch2021/project/controller/US011Controller.java
+++ src/main/java/switch2021/project/controller/US011Controller.java
@@ -8,6 +8,16 @@ public class US011Controller {
     private Company company;
     private SystemUser user;
 
-}
+    public US011Controller (Company company, SystemUser user) {
+        this.company = company;
+        this.user = user;
+    }
+
+    public boolean changePassword(String email, String oldpasswordUI, String newpassword) {
+        this.company.getUserByEmail(email);
+        this.user.updatePassword(oldpasswordUI, newpassword);
+        this.company.saveSystemUser(user);
+    }
+    }
 
 
