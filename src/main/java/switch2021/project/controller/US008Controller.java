17a08fac53724a00caac2f344984caf0ec2efc5f
David1211758 <1211758@isep.ipp.pt>
2022-01-07 00:44:32 +0000

    controller US008 updated

--- src/main/java/switch2021/project/controller/US008Controller.java
+++ src/main/java/switch2021/project/controller/US008Controller.java
@@ -1,25 +1,44 @@
 package switch2021.project.controller;
 
+import jdk.vm.ci.meta.Local;
 import switch2021.project.model.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class US008Controller {
 
     private Company company;
     private Project project;
-    private SystemUser user;
-    private Resource resource;
-    private Profile profile;
+    private Sprint sprint;
+    private ProjectStatus status;
 
-    public List<Project> getProjectList() {
-        return company.getArrayProj();
+    public US008Controller(Company company, Project project, Sprint sprint){
+        this.company = company;
+        this.project = project;
+        this.sprint = sprint;
+        this.status = status;
+
     }
 
+    public Project getProjectRequested(int index){
+        return this.company.getProj(index);
+    }
 
-//    public getProjectRelatedInfo() {
-//
-//   }
+    public Project editProject(String name, LocalDate startDate, LocalDate endDate, int numberOfSprints, int sprintDuration, String status){
+        this.project.setProjectName(name);
+        this.project.setStartDate(startDate);
+        this.project.setEndDate(endDate);
+        this.project.setNumberOfSprints(numberOfSprints);
+//        this.sprint.setDuration(sprintDuration);
+//        this.status.setDescription(status);
+        return this.project;
+    }
+
+    public boolean saveProject(Project project){
+//        this.project.saveProject(project);
+        return true;
+    }
 
 
 
