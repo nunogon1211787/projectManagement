732516d4a984a6126b2a1100d447d3f5d5f31ba3
Cris_Dani <1211756@isep.ipp.pt>
2022-01-07 02:03:08 +0000

    alterei metodo saveSystemUserData da Ju

--- src/main/java/switch2021/project/controller/US010Controller.java
+++ src/main/java/switch2021/project/controller/US010Controller.java
@@ -25,7 +25,7 @@ public class US010Controller {
         }
 
         public boolean saveSystemUser() {
-            return this.company.saveSystemUser(user);
+            return this.company.saveSystemUserData(user);
         }
 
 }
