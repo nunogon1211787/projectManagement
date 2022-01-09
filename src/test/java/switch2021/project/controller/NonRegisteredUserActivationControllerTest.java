e39cfbe1ab4e6e1bde3a190dfb7088a7c0d71ece
anamariaguedes <1211748@isep.ipp.pt>
2022-01-09 21:05:17 +0000

    activationUser

---
+++ src/test/java/switch2021/project/controller/NonRegisteredUserActivationControllerTest.java
@@ -0,0 +1,21 @@
+package switch2021.project.controller;
+
+import org.junit.jupiter.api.Test;
+import switch2021.project.model.Company;
+import switch2021.project.model.SystemUser;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+class NonRegisteredUserActivationControllerTest {
+
+    @Test
+    public void Mytest () {
+        //Arrange
+        Company company = new Company();
+        SystemUser user = new SystemUser("Ana", "1211748@isep.ipp.pt",
+                "User_12", "111", company.getProfile("visitor"));
+        NonRegisteredUserActivationController userActivation = new NonRegisteredUserActivationController(company);
+        //Assert
+        assertTrue(userActivation.getUserbyEmail("1211748@isep.ipp.pt"));
+    }
+}
