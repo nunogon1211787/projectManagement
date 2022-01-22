d535fe667b17894b2c8e69244d1ee32f804bfb77
David1211758 <1211758@isep.ipp.pt>
2022-01-22 22:27:39 +0000

    update tests

---
+++ src/test/java/switch2021/project/model/UserStoryTest.java
@@ -0,0 +1,16 @@
+package switch2021.project.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserStoryTest {
+
+    @Test
+    void setPriorityTest() {
+    }
+
+    @Test
+    void setDescriptionTest() {
+    }
+}
