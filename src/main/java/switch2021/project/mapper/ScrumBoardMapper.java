4706b902440f904a6f4fce35c102d61b21477b4b
Paulo Cunha <1211789@isep.ipp.pt>
2022-01-31 17:06:01 +0000

    US30 Controller

--- src/main/java/switch2021/project/mapper/ScrumBoardMapper.java
+++ src/main/java/switch2021/project/mapper/ScrumBoardMapper.java
@@ -1,13 +1,24 @@
 package switch2021.project.mapper;
 
 import switch2021.project.dto.ScrumBoardDTO;
+import switch2021.project.dto.UserStoryDto;
 import switch2021.project.model.UserStory;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ScrumBoardMapper {
-    public static ScrumBoardDTO toDTO (UserStory userStory) {
+
+    public ScrumBoardDTO toDTO (UserStory userStory) {
         return new ScrumBoardDTO(userStory.getName(),userStory.getUserStoryStatus().getDescription(),
                 userStory.getDescription(), userStory.getTimeEstimate(), userStory.getPriority());
     }
 
-
+    public List<ScrumBoardDTO> toDtoList(List<UserStory> userStoryList){
+        List<ScrumBoardDTO> userStoryDtoList = new ArrayList<>();
+        for(UserStory i: userStoryList) {
+            userStoryDtoList.add(toDTO(i));
+        }
+        return userStoryDtoList;
+    }
 }
