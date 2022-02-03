2b6d10a89aa4a7cde41ddc659a574a2094d9dc7f
David1211758 <1211758@isep.ipp.pt>
2022-02-03 17:02:33 +0000

    US031 DS and DC

---
+++ src/test/java/switch2021/project/stores/ProjectRoleStoreTest.java
@@ -0,0 +1,31 @@
+package switch2021.project.stores;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectRoleStoreTest {
+
+    @Test
+    void createProjectRole() {
+//        ProjectRoleStore projectRoleStore = new ProjectRoleStore();
+//        ProjectRole projRole = projectRoleStore.createProjectRole("Motorista");
+
+    }
+
+    @Test
+    void addProjectRole() {
+    }
+
+    @Test
+    void getProjectRolesList() {
+    }
+
+    @Test
+    void getProjectRole() {
+    }
+
+    @Test
+    void testGetProjectRole() {
+    }
+}
