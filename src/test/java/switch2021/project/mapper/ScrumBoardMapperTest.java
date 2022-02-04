915ee44d81787c76d65d8bc3b5a509807ae075f3
Paulo Cunha <1211789@isep.ipp.pt>
2022-02-04 16:45:57 +0000

    ScrumBoarDTO override test

--- src/test/java/switch2021/project/mapper/ScrumBoardMapperTest.java
+++ src/test/java/switch2021/project/mapper/ScrumBoardMapperTest.java
@@ -3,6 +3,8 @@ package switch2021.project.mapper;
 import org.junit.jupiter.api.Test;
 import switch2021.project.dto.ScrumBoardDTO;
 import switch2021.project.model.UserStory;
+import switch2021.project.stores.UserStoryStatusStore;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ScrumBoardMapperTest {
@@ -51,18 +53,30 @@ public class ScrumBoardMapperTest {
     @Test
     public void overrideTest() {
         //Arrange
+        UserStoryStatusStore store = new UserStoryStatusStore();
+        store.populateDefault();
         ScrumBoardMapper mapper = new ScrumBoardMapper();
         UserStory userStory = new UserStory("name", 5, "description", 5);
         UserStory userStory_2 = new UserStory("name_2", 4, "description_2", 6);
         UserStory userStory_3 = new UserStory("name_2", 4, "description_2", 6);
+        UserStory userStory_4 = new UserStory("name_2", 4, "description_3", 6);
+        userStory_4.setUserStoryStatus(store.getUserStoryStatusByDescription("Done"));
+        UserStory userStory_5 = new UserStory("name_2", 4, "description_3", 6);
+        UserStory userStory_6 = new UserStory("name_asd", 4, "description_3", 6);
 
         //Act
         ScrumBoardDTO dto = mapper.toDTO(userStory);
         ScrumBoardDTO dto_2 = mapper.toDTO(userStory_2);
         ScrumBoardDTO dto_3 = mapper.toDTO(userStory_3);
+        ScrumBoardDTO dto_4 = mapper.toDTO(userStory_4);
+        ScrumBoardDTO dto_5 = mapper.toDTO(userStory_5);
+        ScrumBoardDTO dto_6 = mapper.toDTO(userStory_6);
 
         //Assert
         assertNotEquals(dto,dto_2);
+        assertNotEquals(dto_4,dto_3);
+        assertNotEquals(dto_4,dto_5);
+        assertNotEquals(dto_6,dto_5);
         assertEquals(dto_2,dto_3);
         assertEquals(dto.getClass(),dto_2.getClass());
     }
