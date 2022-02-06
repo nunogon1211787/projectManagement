4e8b59e263243a0daf18bad472b000de55456c5f
anamariaguedes <1211748@isep.ipp.pt>
2022-02-06 00:50:51 +0000

    email change

--- src/test/java/switch2021/project/controller/ActivateUserControllerTest.java
+++ src/test/java/switch2021/project/controller/ActivateUserControllerTest.java
@@ -14,12 +14,12 @@ public class ActivateUserControllerTest {
         //Arrange
         Company company = new Company();
         ActivateUserController controller = new ActivateUserController(company);
-        SystemUser user = new SystemUser("Ana", "1211748@isep.ipp.pt",
+        SystemUser user = new SystemUser("Ana", "xxx@isep.ipp.pt",
                 "User_12", "111", "111", "",company.getUserProfileStore().getUserProfile("Visitor"));
         company.getSystemUserStore().saveSystemUser(user);
 
         // Act
-        boolean result= controller.activateUser("1211748@isep.ipp.pt");
+        boolean result= controller.activateUser("xxx@isep.ipp.pt");
 
         //Assert
         assertTrue(result);
