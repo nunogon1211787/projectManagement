d07233c00f314bef856393d280241c8b54490fe2
Paulo Cunha <1211789@isep.ipp.pt>
2022-02-06 23:46:03 +0000

    Rumo aos 95

--- src/test/java/switch2021/project/model/TaskStatusTest.java
+++ src/test/java/switch2021/project/model/TaskStatusTest.java
@@ -195,4 +195,25 @@ class TaskStatusTest {
         //Assert
         assertTrue(result);
     }
+
+    @Test
+    void overrideTests() {
+        //Arrange
+        TaskStatus status1= new TaskStatus("test1");
+        TaskStatus status2 = new TaskStatus("test2");
+        TaskStatus status3 = new TaskStatus("test3");
+        TaskStatus status4 = null;
+        TaskStatus status5= new TaskStatus("test1");
+        status1.setIDTaskStatus(1);
+        status2.setIDTaskStatus(2);
+        status3.setIDTaskStatus(3);
+        status5.setIDTaskStatus(55);
+        //Assert
+        assertNotEquals(1, status2.getIdTaskStatus());
+        assertNotEquals(2, status3.getIdTaskStatus());
+        assertNotEquals(3, status1.getIdTaskStatus());
+        assertEquals(status1.getClass(),status2.getClass());
+        assertNull(status4);
+        assertNotEquals(status5,status1);
+    }
 }
