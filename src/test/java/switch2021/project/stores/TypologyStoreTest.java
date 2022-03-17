f9cd3c4abf4a14a0dff02bf5658b38e753df4d8e
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-03-17 12:11:08 +0000

    US001: Refactoring: Applied Class "Description" to TypologyStore and corrected unitary tests.

--- src/test/java/switch2021/project/stores/TypologyStoreTest.java
+++ src/test/java/switch2021/project/stores/TypologyStoreTest.java
@@ -6,51 +6,40 @@ import switch2021.project.Immutables.Description;
 import switch2021.project.model.Company;
 import switch2021.project.model.Typology;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TypologyStoreTest {
 
-   /** Attribute **/
-    private Company company;
-    private TypologyStore typologyStore;
-
-    @DisplayName("Test typology list and equal to a different list")
+    @DisplayName("Test typology Store populated and empty store - ")
     @Test
-    public void typologyStoreTest() {
+    public void typologyStoreTest2() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
         TypologyStore test = new TypologyStore();
+        TypologyStore test2 = new TypologyStore();
+        test2.populateDefault();
         //Assert
         assertEquals( 0, test.getTypologyList().size());
-        assertNotEquals(test, typologyStore.getTypologyList());
+        assertNotEquals(test, test2);
     }
 
+    @DisplayName("Test Populate Default")
     @Test
     public void populateTypologyList() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
         TypologyStore test = new TypologyStore();
+        TypologyStore test2 = new TypologyStore();
         Typology tes1 = new Typology("Fixed Cost");
         Typology tes2 = new Typology("Time and Materials");
+        test2.saveTypology(tes1);
+        test2.saveTypology(tes2);
         //Act
         test.populateDefault();
         //Assert
         assertEquals( 2,test.getTypologyList().size());
-        assertEquals(test.getTypologyList(), typologyStore.getTypologyList());
+        assertEquals(test.getTypologyList(), test2.getTypologyList());
         assertEquals(tes1, test.getTypologyByDescription("Fixed Cost"));
         assertEquals(tes2, test.getTypologyByDescription("Time and Materials"));
-    }
-
-    @Test
-    public void createTypologyTestWhitEmptyDescription(){
-        //Assert
-        company = new Company();
-        typologyStore = company.getTypologyStore();
-        assertThrows(IllegalArgumentException.class, () -> {
-            //Arrange and act
-            typologyStore.createTypology("");
-        });
     }
 
     /**
@@ -58,12 +47,17 @@ public class TypologyStoreTest {
      * do método createTypology(), do método saveTypology() e do método ValidateTypology()
      **/
 
+    @DisplayName("Test save Typology")
     @Test //Test for adding new typology not null at Typology List checking attributes and list size.
     public void saveTypologyTestNotNull() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
-        Typology typo2 = new Typology("Test");
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
+        Typology typo2 = mock(Typology.class);
+        Description desc = mock(Description.class);
+        when(typo2.getDescription()).thenReturn(desc);
+        when(desc.getText()).thenReturn("Test");
+//        Typology typo2 = new Typology("Test");
         //Act
         typologyStore.saveTypology(typo2);
         //Assert
@@ -71,24 +65,11 @@ public class TypologyStoreTest {
         assertEquals(3, typologyStore.getTypologyList().size());
     }
 
-    @Test //Test for adding new null typology at Typology List checking attributes and list size.
-    public void saveTypologyTestNull() {
-        //Assert
-        company = new Company();
-        typologyStore = company.getTypologyStore();
-        assertThrows (IllegalArgumentException.class, () -> {
-            //Arrange
-            Typology typo2 = new Typology("");
-            //Act
-            typologyStore.saveTypology(typo2);
-        });
-    }
-
+    @DisplayName("Add and save several typologies at same time")
     @Test
     public void addMultiplesTypologiesAtSameTime() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
         Typology typo2 = new Typology("TestTypology2");
         Typology typo3 = new Typology("TestTypology3");
         Typology typo4 = new Typology("TestTypology4");
@@ -101,19 +82,21 @@ public class TypologyStoreTest {
         typologyStore.saveTypology(typo5);
         typologyStore.saveTypology(typo6);
 
-        Description description = typo3.getDescription();
-        Description value_description = new Description("TestTypology3");
+//        Description description = typo3.getDescription();
+//        Description value_description = mock(Description.class);
+//        when(value_description.getText()).thenReturn("TestTypology3");
+//        Description value_description = new Description("TestTypology3");
 
         //Assert
-        assertEquals(description, value_description);
-        assertEquals(7,typologyStore.getTypologyList().size());
+//        assertEquals(description, value_description);
+        assertEquals(5,typologyStore.getTypologyList().size());
     }
 
     @Test
     public void getTypologyWithDescriptionTest(){
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
         // Act
         Typology descriptionTest = typologyStore.getTypologyByDescription("Time and Materials");
         //Assert
@@ -123,8 +106,8 @@ public class TypologyStoreTest {
     @Test
     public void getTypologyWithWrongDescriptionTest(){
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
         // Act
         Typology descriptionTest = typologyStore.getTypologyByDescription("time and materials");
         //Assert
@@ -134,8 +117,8 @@ public class TypologyStoreTest {
     @Test
     public void getTypologyListTest() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
         TypologyStore test = new TypologyStore();
         //Act
         test.populateDefault();
@@ -146,8 +129,8 @@ public class TypologyStoreTest {
     @Test
     public void validateTypology(){
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
         Typology typo = new Typology("test");
         Typology typo1 = new Typology("Tes");
         Typology typo2 = new Typology("Test2");
@@ -155,18 +138,17 @@ public class TypologyStoreTest {
         assertTrue(typologyStore.validateTypology(typo));
         assertTrue(typologyStore.validateTypology(typo1));
         assertTrue(typologyStore.validateTypology(typo2));
-
     }
 
     @Test
     public void thisNotExistTest() {
         //Arrange
-        company = new Company();
-        typologyStore = company.getTypologyStore();
+        TypologyStore typologyStore = new TypologyStore();
+        typologyStore.populateDefault();
         Typology typo = new Typology("Test");
         //Act
-        company.getTypologyStore().saveTypology(typo);
-        int size = company.getTypologyStore().getTypologyList().size();
+        typologyStore.saveTypology(typo);
+        int size = typologyStore.getTypologyList().size();
         //Assert
         assertEquals(3,size);
     }
@@ -175,8 +157,8 @@ public class TypologyStoreTest {
     public void thisExistTest() {
         //Assert
         assertThrows(IllegalArgumentException.class, () -> {
-            company = new Company();
-            typologyStore = company.getTypologyStore();
+            TypologyStore typologyStore = new TypologyStore();
+            typologyStore.populateDefault();
             //Arrange
             Typology typo = new Typology("teste");
             Typology typo1 = new Typology("teste");
@@ -204,5 +186,90 @@ public class TypologyStoreTest {
         assertEquals(list1.hashCode(), list2.hashCode());
         assertNotEquals(list1, list3);
         assertNotEquals(list1.hashCode(), list3.hashCode());
+    }
+
+    @DisplayName("Create Typology Fail")
+    @Test
+    public void createTypoFail(){
+        assertThrows(IllegalArgumentException.class, () -> {
+
+            TypologyStore typo = new TypologyStore();
+        typo.populateDefault();
+        boolean result = typo.createTypology("Fixed Cost");
+        assertFalse(result);
+        });
+    }
+
+    @DisplayName("Create Typology False")
+    @Test
+    public void createTypoFalse(){
+            TypologyStore typo2 = mock(TypologyStore.class);
+            when(typo2.createTypology("Te")).thenReturn(false);
+            assertFalse(typo2.createTypology("Te"));
+    }
+
+//    @Test
+//    public void shouldNotCreateAndAddRepeatedTitleProject() throws Exception
+//    {
+//        // Arrange
+//        String titulo = "Título";
+//
+//        Typology projectDouble = mock( Typology.class );
+//        Description desc = mock(Description.class);
+//        when( projectDouble.getDescription()).thenReturn( desc );
+//        when( desc.getText()).thenReturn( titulo );
+//
+//
+//        TypologyStore projectFactoryDouble =  mock( TypologyStore.class );
+//        when(projectFactoryDouble.createTypology(titulo) ).thenReturn( true );
+//
+//        TypologyStore storeProjectReeng = new TypologyStore();
+//
+//        // should work fine
+//        boolean hasCreated = storeProjectReeng.createTypology( titulo);
+//
+//        // Act + Assert
+//        // throws IllegalArgumentException, because repeated title
+//        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+//            storeProjectReeng.createTypology( titulo);
+//        });
+//
+//        String expectedMessage = "Repeated typology description inserted.";
+//        String actualMessage = exception.getMessage();
+//
+//        assertTrue(actualMessage.contains(expectedMessage));
+//    }
+
+
+
+    @DisplayName("Create Typology True")
+    @Test
+    public void createTypoTrue(){
+        TypologyStore typo = new TypologyStore();
+        typo.populateDefault();
+        boolean result = typo.createTypology("Teste");
+        assertTrue(result);
+    }
+
+    @DisplayName("Override")
+    @Test
+    public void OverrideEquals(){
+        TypologyStore typo = new TypologyStore();
+        typo.populateDefault();
+        TypologyStore typo2 = new TypologyStore();
+        typo2.populateDefault();
+
+        assertEquals(typo, typo2);
+        assertTrue(typo.equals(typo2));
+    }
+
+    @DisplayName("Override not Equal")
+    @Test
+    public void OverrideNotEquals(){
+        TypologyStore typo = new TypologyStore();
+        typo.populateDefault();
+        Typology typo2 = mock(Typology.class);
+
+        assertNotEquals(typo, typo2);
     }
 }
