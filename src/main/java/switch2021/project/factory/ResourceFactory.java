1b89fbcb9576a76adf6bf89dd09580e14e760ec7
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-03-18 16:53:15 +0000

    Refactoring: create ResourceFactory

--- src/main/java/switch2021/project/factory/ResourceFactory.java
+++ src/main/java/switch2021/project/factory/ResourceFactory.java
@@ -6,8 +6,8 @@ import switch2021.project.model.SystemUser;
 import java.time.LocalDate;
 
 public class ResourceFactory {
-    private Resource res;
+
     public Resource createResource(SystemUser user, LocalDate startDate, LocalDate endDate, double costPerHour, double percentageOfAllocation){
-            return this.res = new Resource(user, startDate,endDate,costPerHour,percentageOfAllocation);
+            return new Resource(user, startDate,endDate,costPerHour,percentageOfAllocation);
     }
 }
