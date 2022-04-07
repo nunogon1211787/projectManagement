a834da5cf86deb49ed8b0ffeb04378243f7b3dae
Romulo Granato <1211793@isep.ipp.pt>
2022-04-07 16:41:31 +0100

    Implement: Create Entity, ValueObject and RepositoryTemplate interfaces. reopen #106.

---
+++ src/main/java/switch2021/project/utils/RepositoryTemplate.java
@@ -0,0 +1,24 @@
+package switch2021.project.utils;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+
+public interface RepositoryTemplate extends CrudRepository<Object, Long> {
+
+    /**
+     * The repository should be able to find a object using given Identity.
+     */
+    //Object findObjectById(Id any);
+
+
+    /**
+     * Finds all objects from this repository.
+     */
+    //List<Object> findAll();
+
+    /**
+     * Save a object in the list of the repository.
+     */
+    //boolean save(Object any);
+
+}
