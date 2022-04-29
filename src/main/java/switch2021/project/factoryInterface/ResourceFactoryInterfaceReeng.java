6161abfa498836fb0950827cfe69722ce3fc04ae
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-04-29 15:30:49 +0100

    Refactoring : Resource Aggregate : evaluation of Resource (addresses #130)

---
+++ src/main/java/switch2021/project/factoryInterface/ResourceFactoryInterfaceReeng.java
@@ -0,0 +1,18 @@
+package switch2021.project.factoryInterface;
+
+import switch2021.project.dto.ResourceDTOReeng;
+import switch2021.project.model.Resource.ResourceReeng;
+import switch2021.project.model.Resource.old.Resource;
+import switch2021.project.model.SystemUser.SystemUser;
+import switch2021.project.model.valueObject.CostPerHour;
+import switch2021.project.model.valueObject.PercentageOfAllocation;
+import switch2021.project.model.valueObject.ProjectID;
+import switch2021.project.model.valueObject.SystemUserId;
+
+import java.time.LocalDate;
+
+public interface ResourceFactoryInterfaceReeng {
+
+    ResourceReeng createResource(ResourceDTOReeng resourceDTO);
+
+}
--- src/main/java/switch2021/project/model/Resource/ResourceId.java
+++ src/main/java/switch2021/project/model/Resource/ResourceId.java
@@ -4,14 +4,18 @@ import lombok.Getter;
 import switch2021.project.model.valueObject.ProjectID;
 import switch2021.project.model.valueObject.SystemUserId;
 
+import java.time.LocalDate;
+
 @Getter
 public class ResourceId {
 
     private final SystemUserId user;
     private final ProjectID project;
+    private final LocalDate startDate;
 
-    public ResourceId(SystemUserId user, ProjectID project) {
+    public ResourceId(SystemUserId user, ProjectID project, LocalDate startDate) {
         this.user = user;
         this.project = project;
+        this.startDate = startDate;
     }
 }
