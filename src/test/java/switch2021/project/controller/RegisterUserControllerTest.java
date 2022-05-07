97ce874f9020eaf94a57949f4f1b7a3325a833f4
Nuno Goncalves [1211787] <1211787@isep.ipp.pt>
2022-05-07 16:08:22 +0100

    US001: Implementation: implement integration tests at RegisterUserControllerTest class and RegisterUserServiceTest (addresses #176)

--- src/test/java/switch2021/project/controller/RegisterUserControllerTest.java
+++ src/test/java/switch2021/project/controller/RegisterUserControllerTest.java
@@ -1,128 +1,51 @@
 package switch2021.project.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import switch2021.project.controller.RegisterUserController;
 import switch2021.project.dto.NewUserInfoDTO;
-import switch2021.project.factory.*;
-import switch2021.project.mapper.SystemUserMapper;
-import switch2021.project.repositories.SystemUserRepository;
-import switch2021.project.service.RegisterUserService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 public class RegisterUserControllerTest {
-    @Mock
-    RegisterUserService registerUserService;
 
-    @InjectMocks
-    RegisterUserController underTest;
+    @Autowired
+    RegisterUserController registerUserController;
 
-    @BeforeEach
-    public void setUp()/* throws Exception*/ {
-        MockitoAnnotations.openMocks(this);
-    }
-
-    @Test
-    @DisplayName("registerUser(NewUserInfoDTO infoDTO)")
-    void itShouldRegisterAUser() {
-        //Arrange
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        NewUserInfoDTO dto = new NewUserInfoDTO();
-        dto.userName = "manueloliveira";
-        dto.email = "manueloliveira@beaver.com";
-        dto.password = "Qwerty_1";
-        dto.passwordConfirmation = "Qwerty_1";
-        dto.function = "tester";
-        dto.photo = "photo.png";
-        //Act
-        ResponseEntity<Object> responseEntity = underTest.registerUser(dto);
-        //Assert
-        assertEquals(201, responseEntity.getStatusCodeValue());
-    }
-
-    //Integration Tests
-    @Test
-    @DisplayName("registerUser(NewUserInfoDTO infoDTO)")
+    @Test //Integration Test
+    @DisplayName(".registerUser(NewUserInfoDTO infoDTO)")
     void itShouldRegisterAUserIntegration() {
         //Arrange
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-        EmailFactory emailFactory = new EmailFactory();
-        SystemUserIDFactory idFactory = new SystemUserIDFactory(emailFactory);
-        NameFactory nameFactory = new NameFactory();
-        FunctionFactory functionFactory = new FunctionFactory();
-        PasswordFactory passwordFactory = new PasswordFactory();
-        PhotoFactory photoFactory = new PhotoFactory();
-        DescriptionFactory descriptionFactory = new DescriptionFactory();
-        UserProfileIDFactory userProfileIDFactory = new UserProfileIDFactory(descriptionFactory);
-        SystemUserFactory factory = new SystemUserFactory(idFactory, nameFactory, functionFactory, passwordFactory,
-                photoFactory,userProfileIDFactory);
-        SystemUserRepository systemUserRepository = new SystemUserRepository();
-        SystemUserMapper mapper = new SystemUserMapper();
-
-
-        RegisterUserService service = new RegisterUserService(systemUserRepository, mapper, factory);
-
-        RegisterUserController controller = new RegisterUserController(service);
-
         NewUserInfoDTO dto = new NewUserInfoDTO();
-        dto.userName = "manueloliveira";
+        dto.userName = "manuelOliveira";
         dto.email = "manueloliveira@beaver.com";
         dto.password = "Qwerty_1";
         dto.passwordConfirmation = "Qwerty_1";
         dto.function = "tester";
         dto.photo = "photo.png";
         //Act
-        ResponseEntity<Object> responseEntity = controller.registerUser(dto);
+        ResponseEntity<Object> responseEntity = registerUserController.registerUser(dto);
         //Assert
         assertEquals(201, responseEntity.getStatusCodeValue());
     }
 
-/*    @Test
+    @Test
     @DisplayName("registerUser(NewUserInfoDTO infoDTO)")
     void itShouldFailToRegisterAUserPasswordsNotMatch() {
-        assertThrows(IllegalArgumentException.class, () -> {
-            //Arrange
-            MockHttpServletRequest request = new MockHttpServletRequest();
-            RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-
-            EmailFactory emailFactory = new EmailFactory();
-            SystemUserIDFactory idFactory = new SystemUserIDFactory(emailFactory);
-            NameFactory nameFactory = new NameFactory();
-            FunctionFactory functionFactory = new FunctionFactory();
-            PasswordFactory passwordFactory = new PasswordFactory();
-            PhotoFactory photoFactory = new PhotoFactory();
-            SystemUserFactory factory = new SystemUserFactory(idFactory, nameFactory, functionFactory, passwordFactory,
-                    photoFactory);
-            SystemUserRepository systemUserRepository = new SystemUserRepository();
-            SystemUserMapper mapper = new SystemUserMapper();
-            RegisterUserService service = new RegisterUserService(systemUserRepository, mapper, factory);
-            RegisterUserController controller = new RegisterUserController(service);
-
-            NewUserInfoDTO dto = new NewUserInfoDTO();
-            dto.userName = "manueloliveira";
-            dto.email = "manueloliveira@beaver.com";
-            dto.password = "Qwerty_1";
-            dto.passwordConfirmation = "Qwerty_2";
-            dto.function = "tester";
-            dto.photo = "photo.png";
-            //Act
-            controller.registerUser(dto);
-        });
+        //Arrange
+        NewUserInfoDTO dto = new NewUserInfoDTO();
+        dto.userName = "manuelOliveira";
+        dto.email = "manueloliveira@beaver.com";
+        dto.password = "Qwerty_1";
+        dto.passwordConfirmation = "Qwerty_2";
+        dto.function = "tester";
+        dto.photo = "photo.png";
+        //Act
+        ResponseEntity<Object> responseEntity = registerUserController.registerUser(dto);
+        //Assert
+        assertEquals(400, responseEntity.getStatusCodeValue());
     }
-
- */
 }
