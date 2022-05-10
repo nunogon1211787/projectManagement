c32748c949a1f338e82b6f401bffb24c6a322825
Romulo Granato <1211793@isep.ipp.pt>
2022-05-10 11:42:30 +0100

    US028: Implementation: unit tests and organize classes. addresses #181

--- src/main/java/switch2021/project/controller/ResourcesController.java
+++ src/main/java/switch2021/project/controller/ResourcesController.java
@@ -3,23 +3,37 @@ package switch2021.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import switch2021.project.dto.DateDTO;
+import switch2021.project.dto.IdDTO;
 import switch2021.project.dto.OutputResourceDTO;
 import switch2021.project.dto.ResourceDTOReeng;
 import switch2021.project.service.CreateResourceInAProjectService;
+import switch2021.project.service.ShowCurrentProjectTeamService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/resources")
-public class CreateResourceInAProjectController {
+public class ResourcesController {
 
     /**
      * Attributes
      **/
-    @Autowired
-    private CreateResourceInAProjectService createResourceService;
+    @Autowired private CreateResourceInAProjectService createResourceService;
+
+    @Autowired private ShowCurrentProjectTeamService srv;
+
+
+    @GetMapping
+    public ResponseEntity<Object> showCurrentProjectTeam(@RequestParam("project") IdDTO dto, @RequestParam("date") DateDTO dateDto){
+
+        List<OutputResourceDTO> resourcesDto = srv.showCurrentProjectTeam(dto, dateDto);
+
+        return new ResponseEntity<>(resourcesDto, HttpStatus.OK);
+
+    }
+
 
     /**
      * Create a Resource
