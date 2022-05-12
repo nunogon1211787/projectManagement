4e273a48066dabfc205b1be7bc0d6dab707d9ed1
Joana Fangueiro [1211770] <1211770@isep.ipp.pt>
2022-05-12 10:39:56 +0100

    US022 - Implement Mapper, Factory, Service and Controller tests (addresses #167)

--- src/test/java/switch2021/project/controller/CreateSprintControllerTest.java
+++ src/test/java/switch2021/project/controller/CreateSprintControllerTest.java
@@ -5,60 +5,60 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import switch2021.project.controller.CreateSprintController;
 import switch2021.project.dto.NewSprintDTO;
+import switch2021.project.dto.OutPutSprintDTO;
 import switch2021.project.service.CreateSprintService;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 
-@AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 public class CreateSprintControllerTest {
 
-    @InjectMocks
-    //@Autowired
-    CreateSprintController createSprintController;
-    @Mock
-    CreateSprintService createSprintService;
-    //@Mock
-    //OutPutSprintDTO outPutSprintDTO;
-
-
+//    @InjectMocks
+//    CreateSprintController createSprintController;
+//
+//    @Mock
+//    CreateSprintService createSprintService;
+//    @Mock
+//    NewSprintDTO newSprintDTO;
+//    @Mock
+//    OutPutSprintDTO outPutSprintDTO;
+//    @Mock
+//    MockHttpServletRequest mockHttpServletRequest;
+//
+//
 //    @Test
 //    @DisplayName("Test to create a sprint, with success")
 //    public void createSprint_Success() {
 //
 //        // Arrange
-//        MockHttpServletRequest request = new MockHttpServletRequest();
-//        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
-//        NewSprintDTO newSprintDTO = mock(NewSprintDTO.class);
+//        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(mockHttpServletRequest));
+//        when(createSprintService.createAndSaveSprint(newSprintDTO)).thenReturn(outPutSprintDTO);
 //
 //        //Act
-//        when(createSprintService.createAndSaveSprint(newSprintDTO)).thenReturn(outPutSprintDTO);
 //        ResponseEntity<Object> responseEntity = createSprintController.createAndSaveSprint(newSprintDTO);
 //
 //        //Assert
 //        assertEquals(responseEntity.getStatusCodeValue(), 201);
 //    }
 
-    @Test
-    @DisplayName("Create Sprint IT")
-    public void createSprint(){
-        //Arrange
-        String project = "Project_2022_1";
-        String name = "Sprint Name";
-        NewSprintDTO dto = new NewSprintDTO(project, name);
-        //Act
-        ResponseEntity<Object> responseEntity = createSprintController.createAndSaveSprint(dto);
-        //Assert
-        assertEquals(201, responseEntity.getStatusCodeValue());
-    }
+//    @Test
+//    @DisplayName("Create Sprint IT")
+//    public void createSprint(){
+//        //Arrange
+//        String project = "Project_2022_1";
+//        String name = "Sprint Name";
+//        NewSprintDTO dto = new NewSprintDTO(project, name);
+//        //Act
+//        ResponseEntity<Object> responseEntity = createSprintController.createAndSaveSprint(dto);
+//        //Assert
+//        assertEquals(201, responseEntity.getStatusCodeValue());
+//    }
 
 }
