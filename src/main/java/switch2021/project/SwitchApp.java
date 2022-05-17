f4219048c85a8b67fe9997eb53cc931c67829e52
Ivan Aguiar [1211768] <1211768@isep.ipp.pt>
2022-05-17 17:42:06 +0100

    Implementation: TypologyJpa.java, TypologyDomainDataAssembler.java, TypologyJpaRepository.java, TypologyRepositoryReeg.java (resolve #197)

--- src/main/java/switch2021/project/SwitchApp.java
+++ src/main/java/switch2021/project/SwitchApp.java
@@ -5,13 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import switch2021.project.datamodel.TypologyJpa;
-import switch2021.project.dto.TypologyDTO;
-import switch2021.project.model.valueObject.Description;
-import switch2021.project.model.valueObject.TypologyID;
 import switch2021.project.repositories.SystemUserRepositoryInterface;
 import switch2021.project.datamodel.SystemUserJpa;
-import switch2021.project.service.TypologyService;
 
 @SpringBootApplication
 public class SwitchApp implements CommandLineRunner {
@@ -22,8 +17,6 @@ public class SwitchApp implements CommandLineRunner {
 
     @Autowired
     private SystemUserRepositoryInterface sURepository;
-    @Autowired
-    private TypologyService typologyService;
 
     @Override
     public void run(String... args) throws Exception {
@@ -36,8 +29,6 @@ public class SwitchApp implements CommandLineRunner {
             this.sURepository.save(new SystemUserJpa("Tom Cruise", "tom@gmail.com", "actor", "photo.png"));
             this.sURepository.save(new SystemUserJpa("Tony Stark", "tony@gmail.com", "tester", "photo.png"));
 
-//            typologyService.createAndSaveTypology(new TypologyDTO("Fixed Cost"));
-//            typologyService.createAndSaveTypology(new TypologyDTO("Time and Materials"));
         };
     }
 }
