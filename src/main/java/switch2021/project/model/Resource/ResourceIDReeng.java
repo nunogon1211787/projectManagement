c86dfd4f44f1dba1befe4559265fb3a08f045102
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-05-25 10:53:13 +0100

    Resource Refactoring: creation of VO factory (address #130)

--- src/main/java/switch2021/project/model/Resource/ResourceIDReeng.java
+++ src/main/java/switch2021/project/model/Resource/ResourceIDReeng.java
@@ -1,12 +1,17 @@
 package switch2021.project.model.Resource;
 
 import lombok.Getter;
+import switch2021.project.factoryInterface.IResouceIDFactory;
 import switch2021.project.model.valueObject.ProjectID;
 import switch2021.project.model.valueObject.SystemUserID;
+import switch2021.project.model.valueObject.UserStoryID;
+import switch2021.project.utils.ValueObject;
 
 import java.time.LocalDate;
+import java.util.Objects;
+
 @Getter
-public class ResourceIDReeng {
+public class ResourceIDReeng implements ValueObject<ResourceIDReeng> {
 
         private final SystemUserID user;
         private final ProjectID project;
@@ -17,4 +22,25 @@ public class ResourceIDReeng {
             this.project = project;
             this.startDate = startDate;
         }
+
+    @Override
+    public boolean sameValueAs(ResourceIDReeng other) {
+        return other != null &&
+                this.user == other.user &&
+                this.project == other.project &&
+                this.startDate == other.startDate ;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ResourceIDReeng)) return false;
+        ResourceIDReeng that = (ResourceIDReeng) o;
+        return Objects.equals(getUser(), that.getUser()) && Objects.equals(getProject(), that.getProject()) && Objects.equals(getStartDate(), that.getStartDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUser(), getProject(), getStartDate());
+    }
+}
