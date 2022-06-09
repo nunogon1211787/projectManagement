dfc7e565220bb3ef464319b2b541e19faa0fdd29
Joana Fangueiro [1211770] <1211770@isep.ipp.pt>
2022-06-09 18:06:43 +0100

    Implement: implement front-end/react. addresses #226

--- webapp/src/routes/ProtectedRoute.jsx
+++ webapp/src/routes/ProtectedRoute.jsx
@@ -1,17 +1,14 @@
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import AppContext from "../context/AppContext";
-
 const ProtectedRoute = (props) => {
   const { state } = useContext(AppContext);
   const { token } = state.auth.data;
   const location = useLocation();
-
   if (!token) {
     return <Navigate to="/home" replace state={{ from: location }} />;
   }
 
   return props.children;
 };
-
 export default ProtectedRoute;
