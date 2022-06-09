38738af26fc6279136eb6cd326c10801af6743ae
Cristiana Carvalho <1211756@isep.ipp.pt>
2022-06-09 17:51:43 +0100

    Implement: implement front-end/react. (addresses #226)

--- webapp/src/pages/CreateUserStory.jsx
+++ webapp/src/pages/CreateUserStory.jsx
@@ -1,51 +1,71 @@
 import Button from "../components/Button";
-import { useContext } from "react";
+import {useContext} from "react";
 import Form from "../components/Form";
 import Table from "../components/Table";
 import AppContext from "../context/AppContext";
-import { navToForm } from "../context/Actions";
-import { Box } from "grommet";
+import {navToForm, navToTable} from "../context/Actions";
+import {Box} from "grommet";
 
 const postBody = {
-  projectID: "",
-  title: "",
-  priority: "",
-  description: "",
-  timeEstimate: "",
+    projectID: "",
+    title: "",
+    priority: "",
+    description: "",
+    timeEstimate: "",
 };
 
 const inputTypes = ["text", "text", "number", "text", "number"];
 
 export default function CreateUserStory() {
-  const { state, dispatch } = useContext(AppContext);
-  const { navigation } = state;
-  const { table, form } = navigation;
+    const {state, dispatch} = useContext(AppContext);
+    const {navigation} = state;
+    const {table, form, details} = navigation;
 
-  const buttonNavigate = () => {
-    navToForm(dispatch);
-  };
+    const buttonNavigate = () => {
+        navToForm(dispatch);
+    };
 
-  if (table) {
-    return (
-      <Box fill align="center" justify="center">
-        <h1>User Story</h1>
-        <Table collections="userstories" />
-        <Button name="Create User Story" function={buttonNavigate} />
-      </Box>
-    );
-  } else {
-    if (form) {
-      return (
-        <>
-          <Box fill align="center" justify="center">
-            <Form
-              label={postBody}
-              rules={inputTypes}
-              collections="userstories"
-            />
-          </Box>
-        </>
-      );
+    const buttonNavigateEdit = () => {
+        navToForm(dispatch);
+    };
+
+    const buttonNavigateBack = () => {
+        navToTable(dispatch);
+    };
+
+    if (table) {
+        return (
+            <Box fill align="center" justify="center">
+                <h1>User Story</h1>
+                <Table collections="userstories"/>
+                <Button name="Create User Story" function={buttonNavigate}/>
+            </Box>
+        );
+    } else {
+        if (form) {
+            return (
+                <>
+                    <Box fill align="center" justify="center">
+                        <Form
+                            label={postBody}
+                            rules={inputTypes}
+                            collections="userstories"
+                        />
+                    </Box>
+                </>
+            );
+        } else {
+            if (details) {
+                return (
+                    <>
+                        <h1>User Story</h1>
+                        {/*{<Details details={usID}/>}*/}
+                        <Button name="Edit User Story" function={buttonNavigateEdit}/>
+                        <Button name="Back to user stories list" function={buttonNavigateBack}/>
+                    </>
+                )
+
+            }
+        }
     }
-  }
 }
