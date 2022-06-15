226a9716799d0ae5ffe7fc8d93be26734bd9314b
Joana Fangueiro [1211770] <1211770@isep.ipp.pt>
2022-06-15 17:12:40 +0100

    Implementation : WebClient (resolve #250)

--- src/main/java/switch2021/project/dataModel/REST/assemblers/UserProfileDomainDataRestAssembler.java
+++ src/main/java/switch2021/project/dataModel/REST/assemblers/UserProfileDomainDataRestAssembler.java
@@ -1,4 +1,37 @@
 package switch2021.project.dataModel.REST.assemblers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import switch2021.project.dataModel.REST.UserProfileRestDTO;
+import switch2021.project.entities.aggregates.UserProfile.UserProfile;
+import switch2021.project.entities.valueObjects.voFactories.voFactories.UserProfileIDFactory;
+import switch2021.project.entities.valueObjects.vos.UserProfileID;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
 public class UserProfileDomainDataRestAssembler {
+
+    @Autowired
+    UserProfileIDFactory userProfileIDFactory;
+
+
+    public UserProfile toDomain(UserProfileRestDTO userProfileRestDTO) {
+
+        UserProfileID userProfileID = userProfileIDFactory.createUserProfileID(userProfileRestDTO.getDescription());
+
+        return new UserProfile(userProfileID);
+    }
+
+    public List<UserProfile> toCollections(List<UserProfileRestDTO> userProfileRestDTO) {
+
+        List<UserProfile> newList = new ArrayList<>();
+
+        userProfileRestDTO.forEach(uProfile -> newList.add(toDomain(uProfile)));
+
+        return newList;
+    }
+
 }
