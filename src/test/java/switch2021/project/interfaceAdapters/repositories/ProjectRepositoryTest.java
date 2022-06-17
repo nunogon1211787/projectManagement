e7e3b4133fb569718e4cf2931fb96cd47df581e0
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-06-17 16:34:25 +0100

    Refactoring: Improve test coverage ProjectRepository(address #182)

--- src/test/java/switch2021/project/interfaceAdapters/repositories/ProjectRepositoryTest.java
+++ src/test/java/switch2021/project/interfaceAdapters/repositories/ProjectRepositoryTest.java
@@ -1,6 +1,8 @@
 package switch2021.project.interfaceAdapters.repositories;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
@@ -8,9 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import switch2021.project.dataModel.JPA.ProjectJpa;
 import switch2021.project.dataModel.JPA.assembler.ProjectJpaAssembler;
+import switch2021.project.entities.valueObjects.vos.Password;
 import switch2021.project.persistence.ProjectJpaRepository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
 //@ExtendWith(SpringExtension.class)
@@ -32,6 +37,20 @@ public class ProjectRepositoryTest {
     }
 
     //
+    @DisplayName("Save success")
+    @Test
+    public void saveSuccess() {
+        //Arrange
+        ProjectJpa projJpa = mock(ProjectJpa.class);
+
+        //Act
+
+        //Assert
+
+
+    }
+
+
 
 
 }
