931d962b61e4cc484ab57013e6311e23b0cfad93
Cristiana Carvalho <1211756@isep.ipp.pt>
2022-06-19 22:30:44 +0100

    Resolve merge conflits (addresses #242)

--- src/main/java/switch2021/project/applicationServices/iRepositories/IProjectWebRepository.java
+++ src/main/java/switch2021/project/applicationServices/iRepositories/IProjectWebRepository.java
@@ -1,11 +1,9 @@
 package switch2021.project.applicationServices.iRepositories;
 
-import org.springframework.stereotype.Repository;
 import switch2021.project.entities.aggregates.Project.Project;
 
 import java.util.List;
 
-@Repository
 public interface IProjectWebRepository {
 
     List<Project> findAll();
--- src/main/java/switch2021/project/interfaceAdapters/repositories/ProjectWebRepository.java
+++ src/main/java/switch2021/project/interfaceAdapters/repositories/ProjectWebRepository.java
@@ -1,12 +1,14 @@
 package switch2021.project.interfaceAdapters.repositories;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import switch2021.project.applicationServices.iRepositories.IProjectWebRepository;
 import switch2021.project.dataModel.REST.assemblers.ProjectDomainDataRestAssembler;
 import switch2021.project.entities.aggregates.Project.Project;
 import switch2021.project.interfaceAdapters.repositories.REST.ProjectRestRepository;
 import java.util.List;
 
+@Repository
 public class ProjectWebRepository implements IProjectWebRepository {
 
     @Autowired
