b7062357c2e7080844d02877dd00a37ad42fff47
David Goncalves [1211758] <1211758@isep.ipp.pt>
2022-06-22 09:46:42 +0100

    Implement: implement unitary tests to UserProfileController(addresses #252)

--- src/test/java/switch2021/project/interfaceAdapters/controller/UserProfileControllerTest.java
+++ src/test/java/switch2021/project/interfaceAdapters/controller/UserProfileControllerTest.java
@@ -12,14 +12,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import switch2021.project.applicationServices.service.UserProfileService;
+import switch2021.project.dtoModel.dto.EditProjectInfoDTO;
+import switch2021.project.dtoModel.dto.OutputProjectDTO;
+import switch2021.project.dtoModel.dto.ProjectDTO;
 import switch2021.project.dtoModel.dto.UserProfileDTO;
+import switch2021.project.entities.aggregates.Project.Project;
+import switch2021.project.entities.valueObjects.vos.Budget;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @SpringBootTest
@@ -43,10 +48,10 @@ public class UserProfileControllerTest {
         UserProfileDTO test = mock(UserProfileDTO.class);
         UserProfileDTO test2 = mock(UserProfileDTO.class);
         UserProfileDTO test3 = mock(UserProfileDTO.class);
-        when(service.showAllProfiles()).thenReturn(CollectionModel.of
+        when(service.getAllProfiles()).thenReturn(CollectionModel.of
                 (List.of(new UserProfileDTO[]{test, test2, test3})));
         //Act
-        ResponseEntity<?> response = ctrl.showAllProfiles();
+        ResponseEntity<?> response = ctrl.getAllProfiles();
         //Assert
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
     }
@@ -54,9 +59,9 @@ public class UserProfileControllerTest {
     @Test
     void getAllProfilesCatchException() {
         //Arrange
+        when(service.getAllProfiles()).thenThrow();
         //Act
-        when(service.showAllProfiles()).thenThrow();
-        ResponseEntity<?> response = ctrl.showAllProfiles();
+        ResponseEntity<?> response = ctrl.getAllProfiles();
         //Assert
         assertThat(response.getStatusCodeValue()).isEqualTo(400);
     }
@@ -67,8 +72,8 @@ public class UserProfileControllerTest {
         //Arrange
         UserProfileDTO test = mock(UserProfileDTO.class);
         String x = "Fixe";
-        test.setDescription(x);
-        when(service.showUserProfileRequested(x)).thenReturn(test);
+        test.setUserProfileName(x);
+        when(service.findUserProfileRequested(x)).thenReturn(test);
         //Act
         ResponseEntity<?> response = ctrl.showUserProfileRequested(x);
         //Assert
@@ -79,13 +84,64 @@ public class UserProfileControllerTest {
     @Test
     void getProfileRequestedCatchException() {
         //Arrange
-        when(service.showUserProfileRequested(anyString())).thenThrow();
+        when(service.findUserProfileRequested(anyString())).thenThrow();
         //Act
         ResponseEntity<?> response = ctrl.showUserProfileRequested("1");
         //Assert
         assertThat(response.getStatusCodeValue()).isEqualTo(404);
     }
 
+    @SneakyThrows
+    @Test
+    void testCreateProfile() {
+        //Arrange
+        UserProfileDTO test = mock(UserProfileDTO.class);
+        test.userProfileName = "Adeus";
+        UserProfileDTO outTest= mock(UserProfileDTO.class);
+        when(service.createAndSaveUserProfile(test)).thenReturn(outTest);
+        //Act
+        ResponseEntity<?> response = ctrl.createUserProfile(test);
+        //Assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(201);
+    }
+
+    @SneakyThrows
+    @Test
+    void testCreateProfileException() {
+        //Arrange
+        UserProfileDTO test = mock(UserProfileDTO.class);
+        test.userProfileName = "Adeus";
+        when(service.createAndSaveUserProfile(test)).thenThrow(Exception.class);
+        //Act
+        ResponseEntity<?> response = ctrl.createUserProfile(test);
+        //Assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(400);
+    }
+
+    @SneakyThrows
+    @Test
+    void testUpdateProfile() {
+        //Arrange
+        UserProfileDTO test = mock(UserProfileDTO.class);
+        UserProfileDTO outTest= mock(UserProfileDTO.class);
+        when(service.editARequestedUserProfile("1", test)).thenReturn(outTest);
+        //Act
+        ResponseEntity<?> response = ctrl.editAUserProfile("1", test);
+        //Assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(202);
+    }
+
+    @SneakyThrows
+    @Test
+    void testUpdateProfileException() {
+        //Arrange
+        UserProfileDTO test = mock(UserProfileDTO.class);
+        when(service.editARequestedUserProfile(anyString(),any())).thenThrow(Exception.class);
+        //Act
+        ResponseEntity<?> response = ctrl.editAUserProfile("1", test);
+        //Assert
+        assertThat(response.getStatusCodeValue()).isEqualTo(400);
+    }
 
 
 
