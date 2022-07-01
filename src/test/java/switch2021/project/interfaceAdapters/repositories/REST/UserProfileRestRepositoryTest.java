bdb2c21ad1ac5d08b7749480c993a56ca93fda75
Ivan Aguiar [1211768] <1211768@isep.ipp.pt>
2022-07-01 12:23:16 +0100

    Refactoring: refactoring unit tests;
    (address #182)

--- src/test/java/switch2021/project/interfaceAdapters/repositories/REST/UserProfileRestRepositoryTest.java
+++ src/test/java/switch2021/project/interfaceAdapters/repositories/REST/UserProfileRestRepositoryTest.java
@@ -1,31 +1,32 @@
 package switch2021.project.interfaceAdapters.repositories.REST;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
-public class ProjectRestRepositoryTest {
-
+class UserProfileRestRepositoryTest {
 
     @Test
-    void doTestForceMonoError() {
+    void doTestForceMonoError(){
         //Assert
         assertThrows(NullPointerException.class, () -> {
-            String ENDPOINT = "https://vs866.dei.isep.ipp.pt:8443/switchproject-1.0-SNAPSHOT/api";
-            String COLLECTION = "/projects";
+            String endpoint = "https://vs866.dei.isep.ipp.pt:8443/switchproject-1.0-SNAPSHOT/api";
+            String collection = "/projects";
             WebClient webClient = mock(WebClient.class);
             WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock = mock(WebClient.RequestHeadersUriSpec.class);
             when(webClient.get()).thenReturn(requestHeadersUriSpecMock);
             WebClient.RequestBodySpec requestBodySpec = mock(WebClient.RequestBodySpec.class);
-            when(requestHeadersUriSpecMock.uri(ENDPOINT + COLLECTION)).thenReturn(requestBodySpec);
+            when(requestHeadersUriSpecMock.uri(endpoint + collection)).thenReturn(requestBodySpec);
             WebClient.ResponseSpec responseSpec = mock(WebClient.ResponseSpec.class);
             when(requestBodySpec.retrieve()).thenReturn(responseSpec);
             when(responseSpec.onStatus(HttpStatus::is4xxClientError, error -> Mono.empty())).thenThrow(NullPointerException.class);
-            ProjectRestRepository repository = new ProjectRestRepository();
+            UserProfileRestRepository repository = new UserProfileRestRepository();
             //Act
             repository.findAll();
         });
