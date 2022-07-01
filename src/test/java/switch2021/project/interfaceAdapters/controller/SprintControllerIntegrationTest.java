4f5d9c0a44247fbb40e77cc3e483e689198ac978
Carolina Silva [1211753] <1211753@isep.ipp.pt>
2022-07-01 15:37:30 +0100

    Refactoring: ResourceController: Integration tests (addresses #275)

--- src/test/java/switch2021/project/interfaceAdapters/controller/SprintControllerIntegrationTest.java
+++ src/test/java/switch2021/project/interfaceAdapters/controller/SprintControllerIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class SprintControllerIntegrationTest {
     public static final String BASE_URL = "https://localhost:8443";
 
